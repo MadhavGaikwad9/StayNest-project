02fb921e1b11ca0a2e9d4dee73955c39cca8078d
MadhavGaikwad9 <madhavgaikwad98476@gmail.com>
2026-02-23 00:21:26 +0530

    styling to reviews

--- schema.js
+++ schema.js
@@ -15,6 +15,6 @@ module.exports.listingSchema = joi.object({
 module.exports.reviewSchema = joi.object({
     review:joi.object({
         rating:joi.number().required().min(1).max(5),
-        comment:joi.string().require(),
-    }).require(),
+        comment:joi.string().required(),
+    }).required(),
 });
